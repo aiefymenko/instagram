0bf907b8e2fd8bc9c82216114119450d37f47d91
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-11-24 20:50:36 -0500

    Seed the db to firestore

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,13 +3,22 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
 //import seed files
+// import { seedDatabase } from '../seed';
 
-const config = {};
+const config = {
+  apiKey: "AIzaSyC-q4D1Sb9Z-a-vTrzcrfIqYpGC9k4-E_4",
+  authDomain: "instagram-ai.firebaseapp.com",
+  projectId: "instagram-ai",
+  storageBucket: "instagram-ai.appspot.com",
+  messagingSenderId: "619871278904",
+  appId: "1:619871278904:web:8017625d0849ff748253cd"
+};
 
 const firebase = Firebase.initializeApp(config);
 const { FieldValue } = Firebase.firestore;
 
 
 // calling seed files (ONCE!!!)
+// seedDatabase(firebase);
 
 export {firebase, FieldValue};
