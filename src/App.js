973cf36d9674d74a753c4be14484d9a99fa99b2f
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-11-25 21:10:15 -0500

    Add lazyloading to login page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
-//loading login page at the background in "lazy" was
+//loading login page at the background in "lazy" mode
 const Login = lazy(() => import('./pages/login'));
 
 
 function App() {
   return (
     <Router>
-      <Suspense fallback={<p>Loading...</p>}>
+      <Suspense fallback= {<p>Loading...</p>} >
         <Routes>
-          <Route path="/login" component={Login} />
+          <Route path='/login' element={<Login />} />
         </Routes>
-
       </Suspense>
     </Router>
   );
