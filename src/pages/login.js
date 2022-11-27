557278519d79a511da92df94b95c2ca2e09562ae
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-11-26 21:36:51 -0500

    Create states for ling page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import FirebaseContxt from '../context/firebase'
 
-export default function login() {
+export default function Login() {
+  const history = useNavigate();
+  const { firebase } = useContext(FirebaseContxt);
+
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [error, setErrro] = useState('');
+  const isInvalid = password === '' || emailAddress === '';
+
+  const handleLogin = () => {
+
+  }
+
+  useEffect(() => {
+    document.title = 'Login-instagram'
+  }, []);
+
   return (
     <div>login is here</div>
   )
