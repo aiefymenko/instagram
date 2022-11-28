a52bebcb856aa6782c2f554b52bd4c8ee6670f5c
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-11-27 20:49:35 -0500

    Fix minor issue

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,10 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import FirebaseContxt from '../context/firebase'
+import FirebaseContext from '../context/firebase'
 
 export default function Login() {
   const history = useNavigate();
-  const { firebase } = useContext(FirebaseContxt);
+  const { firebase } = useContext(FirebaseContext);
 
   const [emailAddress, setEmailAddress] = useState('');
   const [password, setPassword] = useState('');
