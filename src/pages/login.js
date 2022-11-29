35f4dbfb710cb89fd09b6e28c205a87bef1ad366
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-11-28 21:40:48 -0500

    Add profile/login picture to test

--- src/pages/login.js
+++ src/pages/login.js
@@ -21,6 +21,10 @@ export default function Login() {
   }, []);
 
   return (
-    <div>login is here</div>
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+    <div className="flex w-3/5 h-1/2">
+      <img src="/images/iphone-image.png" alt="Iphone with profile" />
+    </div>
+    </div>
   )
 }
