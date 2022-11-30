5f785ef3e41737341198fc8e6a910f9a15c0d634
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-11-29 20:38:43 -0500

    Add instagram logo and start creating form

--- src/pages/login.js
+++ src/pages/login.js
@@ -21,9 +21,15 @@ export default function Login() {
   }, []);
 
   return (
-    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-    <div className="flex w-3/5 h-1/2">
+    <div className="container flex  mx-auto max  -w-screen-md items-center h-screen">
+    <div className="flex justify-end w-3/5 h-1/2">
       <img src="/images/iphone-image.png" alt="Iphone with profile" />
+    </div>
+    <div className="flex flex-col w-2/5">
+      <h1 className='flex justify-center w-full'>
+        <img src='/images/logo.png' alt = 'Instagram logo' />
+        </h1>
+        { error && <p className='mb-4 text-xs text-red-primary'>{error}</p>}
     </div>
     </div>
   )
