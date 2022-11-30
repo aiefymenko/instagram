0bec9f953d7eaafc1c719931237c9422ba3fa810
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-11-29 20:49:52 -0500

    Add email input with styling

--- src/pages/login.js
+++ src/pages/login.js
@@ -30,6 +30,16 @@ export default function Login() {
         <img src='/images/logo.png' alt = 'Instagram logo' />
         </h1>
         { error && <p className='mb-4 text-xs text-red-primary'>{error}</p>}
+
+        <form onSubmit={handleLogin} method="POST">
+          <input
+          aria-label='Enter your email address'
+          type = 'text'
+          placeholder='Email address'
+          className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border-gray-primery rounded mb-2'
+          onChange={({target}) => console.log(target.value)}
+          />
+        </form>
     </div>
     </div>
   )
