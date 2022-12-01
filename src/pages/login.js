33c7f994397b7b6c5b0183afeff8d37334ab12dd
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-11-30 21:03:02 -0500

    Minor styling issues

--- src/pages/login.js
+++ src/pages/login.js
@@ -21,14 +21,14 @@ export default function Login() {
   }, []);
 
   return (
-    <div className="container flex  mx-auto max  -w-screen-md items-center h-screen">
-    <div className="flex justify-end w-1/2 h-5/6">
+    <div className='container flex  mx-auto max-w-screen-md items-center h-screen'>
+    <div className='flex justify-end w-3/5 h-5/6'>
       <img src="/images/iphone-image.png" alt="Iphone with profile" />
     </div>
-    <div className="flex flex-col w-1/2 p-8">
-      <div className='"flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded'>
+    <div className='flex flex-col w-2/5 p-5'>
+      <div className='flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded'>
       <h1 className='flex justify-center w-full'>
-        <img src='/images/logo.png' alt = 'Instagram logo' />
+        <img src='/images/logo.png' alt = 'Instagram logo' className="mt-2 w-6/12 mb-4" />
         </h1>
         { error && <p className='mb-4 text-xs text-red-primary'>{error}</p>}
 
@@ -37,7 +37,7 @@ export default function Login() {
           aria-label='Enter your email address'
           type = 'text'
           placeholder='Email address'
-          className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primery rounded mb-2'
+          className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2'
           onChange={({target}) => setEmailAddress(target.value)}
           />
                     <input
@@ -54,7 +54,7 @@ export default function Login() {
           ${isInvalid && 'opacity-50'}`}> Log In </button>
         </form>
     </div>
-    <div className='flex justify-center items-center flex-column w-full bg-white p-4 rounded border border-gray-primary'>
+    <div className='flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary'>
       <p className='text-sm'>Don't have an account? {``}
       <Link to='/signup' className='font-bold text-blue-medium'>
         Sign Up
