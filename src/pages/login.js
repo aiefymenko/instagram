bce5ae01273ae6acc987b56dc53ef8ca00283c0b
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-11-30 20:07:47 -0500

    Add submit button

--- src/pages/login.js
+++ src/pages/login.js
@@ -37,15 +37,20 @@ export default function Login() {
           type = 'text'
           placeholder='Email address'
           className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border-gray-primery rounded mb-2'
-          onChange={({target}) => console.log(target.value)}
+          onChange={({target}) => setEmailAddress(target.value)}
           />
                     <input
           aria-label='Enter your password'
           type = 'password'
           placeholder='Password'
           className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border-gray-primery rounded mb-2'
-          onChange={({target}) => console.log(target.value)}
+          onChange={({target}) => setPassword(target.value)}
           />
+          <button
+          disabled={isInvalid}
+          type='submit'
+          className={`bg-blue-500 text-white w-full rounded h-8 font-bold
+          ${isInvalid && 'opacity-50'}`}> Log In </button>
         </form>
     </div>
     </div>
