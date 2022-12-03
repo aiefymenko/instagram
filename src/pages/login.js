ad46797a286b8b22816441b02f59f694287cdfe2
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-12-02 20:30:20 -0500

    Fix some styling

--- src/pages/login.js
+++ src/pages/login.js
@@ -53,7 +53,7 @@ export default function Login() {
           className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2'
           onChange={({target}) => setEmailAddress(target.value)}
           />
-                    <input
+          <input
           aria-label='Enter your password'
           type = 'password'
           placeholder='Password'
