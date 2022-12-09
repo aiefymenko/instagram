a95e7786e3b43c1f6527e9b83e7d15646ffd619f
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-12-08 19:14:31 -0500

    Create auth listener hook wheneveer we auth a user

---
+++ src/hooks/use-auth-listener.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect, useContext} from 'react';
+import FirebaseContext from '../context/firebase';
+
+export default function useAuthListener() {
+
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('authUser')));
+  const {firebase} = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const listener = firebase.auth().onAuthStateChange((authUser) => {
+      //we have a user so we can store it local localstorage
+      if (authUser) {
+        localStorage.setItem('authUser', JSON.stringify(authUser));
+        setUser(authUser);
+      } else {
+        // we dom't have authUser then clear localstorage
+        localStorage.removeItem('authUser');
+        setUser(null);
+      }
+      
+    })
+
+    return() => listener();
+
+  }, [firebase])
+
+  return (
+    {user}
+  )
+}
