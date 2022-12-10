ff57c909d9635065cef5c6328af909950e9a5b9c
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-12-10 17:09:12 -0500

    Start working on Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import {useContext} from 'react';
+import { Link } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
+import UserContext from '../context/user';
+import * as ROUTES from '../constants/routes';
 
 export default function Header() {
+  const { firebase } = useContext(FirebaseContext);
+  const { user } = useContext(UserContext);
+
   return (
-    <div>Header</div>
+    <header className='h-16 bg-white border-b border-gray-primary mb-8'>Hello</header>
   )
 }
