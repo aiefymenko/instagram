619c8d9c123065264d25e650d57c2432038d9624
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-12-10 17:28:18 -0500

    Add instagram logo as link to dashboard and some styking

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,17 @@ export default function Header() {
   const { user } = useContext(UserContext);
 
   return (
-    <header className='h-16 bg-white border-b border-gray-primary mb-8'>Hello</header>
+    <header className='h-16 bg-white border-b border-gray-primary mb-8'>
+      <div className='container mx-auto max-w-screen-lg h-full'>
+        <div className='flex justify-between h-full'>
+        <div className='text-gray-700 text-center flex items-center align-items cursor-pointer'>
+          <h1 className='flex justify-center w-full'>
+            <Link to={ROUTES.DASHBOARD}>
+              <img src='/images/logo.png' alt='Instagram' className='mt-2 w-6/12'/> 
+              </Link> </h1>
+        </div>
+        </div>
+      </div>
+    </header>
   )
 }
