4db5e60467aae20866f8880243137f2baa49a8db
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-12-15 20:09:50 -0500

    Create sidebar and bring user

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React from 'react';
+import useUser from '../hooks/use-user';
 
 export default function Sidebar() {
+  const {user: {fullName, username, userId }} = useUser();
+
+  console.log('fullName, username, userId', fullName, username, userId);
   return (
     <div>Sidebar</div>
   )
