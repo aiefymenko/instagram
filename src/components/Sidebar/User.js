af0a5aee3c6d7a165adfa67dbdb96b6e8f1f0318
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-12-18 20:15:41 -0500

    Add wdyr.js for whyDidYouRender

--- src/components/Sidebar/User.js
+++ src/components/Sidebar/User.js
@@ -26,6 +26,6 @@ const User = ({username, fullName}) =>
 export default memo(User);
 
 User.propTypes = {
-  username: PropTypes.string.isRequired,
-  fullName: PropTypes.string.isRequired
+  username: PropTypes.string,
+  fullName: PropTypes.string
 }
