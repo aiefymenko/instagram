b3d76a88ee5b708dcc45bdc833e69723d4a5c88a
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-12-20 16:55:21 -0500

    Pass following as props

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,12 +3,12 @@ import User from './User';
 import Suggestions from './Suggestions';
 
 export default function Sidebar() {
-  const {user: {fullName, username, userId }} = useUser();
+  const {user: {fullName, username, userId, following }} = useUser();
 
   return (
     <div className='p-4'>
       <User username={username} fullName={fullName} /> 
-      <Suggestions userId={userId}/>
+      <Suggestions userId={userId} following={following}/>
     </div>
   )
 }
