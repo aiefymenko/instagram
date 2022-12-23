5cbbbbc6157fd74629d47f0bbeee3052f2bdcb6e
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-12-22 20:17:19 -0500

    Add follow button

--- src/components/Sidebar/SuggestedProfile.js
+++ src/components/Sidebar/SuggestedProfile.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 export default function SuggestedProfile({userDocId, username, profileId, userId }) {
@@ -11,7 +12,16 @@ export default function SuggestedProfile({userDocId, username, profileId, userId
           src={`/images/avatars/${username}.jpg`}
           alt=''
           />
+          <Link to={`/p/${username}`}>
+          <p className='font-bold text-sm'>{username}</p>
+          </Link>
         </div>
+        <button className='text-xs font-bold text-blue-medium'
+        type='button'
+        onClick={() => console.log('Follow this user')}
+        >
+          Follow
+        </button>
       </div>
     ) : null;
 }
