27ea92f02c7d9426d909638eeb4cc94e7b542b79
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-12-26 17:44:57 -0500

    Create state for photos

--- src/hooks/use-photos.js
+++ src/hooks/use-photos.js
@@ -2,7 +2,9 @@ import { useState, useEffect, useContext } from "react";
 import UserContext from "../context/user";
 
 export default function usePhotos() {
+const [photos, setPhotos] = useState(null);
 
+return {photos};
 }
 
 
