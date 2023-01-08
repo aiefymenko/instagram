dbe63e74eb4632c57f44735e8b7a257d0d5591f1
Artem Iefymenko <iefymenko.artem@gmail.com>
2023-01-07 20:46:23 -0500

    Add properties for variables

--- src/components/post/Comments.js
+++ src/components/post/Comments.js
@@ -11,3 +11,11 @@ export default function Comments({docId, comments: allComments, posted, commentI
     </>
   )
 }
+
+Comments.propTypes = {
+  docId: PropTypes.string.isRequired,
+  comments: PropTypes.array.isRequired,
+  posted: PropTypes.number.isRequired,
+  commentInput: PropTypes.object.isRequired,
+
+}
