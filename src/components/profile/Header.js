3d0894e075e96d7a85b7b13bf2a505c343af2d7f
Artem Iefymenko <iefymenko.artem@gmail.com>
2023-01-21 19:52:58 -0500

    Create PropTypes for arguments in Header

--- src/components/profile/Header.js
+++ src/components/profile/Header.js
@@ -2,9 +2,16 @@ import { useState, useEffect } from "react";
 import PropTypes from 'prop-types';
 import Skeleton from "react-loading-skeleton";
 
-export default function Header() {
+export default function Header({photosCount, profile, followerCount, setFollowerCount}) {
   const [isFollowingProfile, setIsFollowingProfile] = useState(false);
   return (
     <div>Header</div>
   )
+}
+
+Header.propTypes = {
+  photosCount: PropTypes.number.isRequired,
+  profile: PropTypes.object.isRequired,
+  followerCount: PropTypes.number.isRequired,
+  setFollowerCount: PropTypes.func.isRequired
 }
