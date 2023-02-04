b23962344c36599c65bf8476bd6ffad2facf0b72
Artem Iefymenko <iefymenko.artem@gmail.com>
2023-02-03 20:43:19 -0500

    Fix an issue with hover

--- src/components/profile/Photos.js
+++ src/components/profile/Photos.js
@@ -14,9 +14,9 @@ export default function Photos({photos}) {
         photos.map((photo) => (
           <div key={photo.docId} className='relative group'>
               <img src={photo.imageSrc} alt={photo.caption} />
-              <div className='absolut bottom-0 left-0 bg-gray-200 z-10 w-full justify-evenly items-center bg-black-faded group-hover: flex'>
-                <p className='flex itmes-center text-white font-bold'>
-                <svg
+              <div className="absolute bottom-0 left-0 bg-gray-200 z-10 w-full justify-evenly items-center h-full bg-black-faded group-hover:flex hidden">
+                  <p className="flex items-center text-white font-bold">
+                    <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 20 20"
                       fill="currentColor"
@@ -46,8 +46,8 @@ export default function Photos({photos}) {
                     </svg>
                     {photo.comments.length}
                   </p>
-                  </div>
-            </div>
+                </div>
+              </div>
         ))
       ) : null}
     </div>
