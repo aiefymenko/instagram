bbe697b49448d36e1adc866887f32d51ab4a40ba
Artem Iefymenko <iefymenko.artem@gmail.com>
2023-02-07 20:54:11 -0500

    Remove console.log

--- src/hooks/use-photos.js
+++ src/hooks/use-photos.js
@@ -10,12 +10,12 @@ async function getTimeLinePhotos() {
   
 //does the user actually follow people
   if (user?.following.length > 0) {
-    let followedUserPhotos = [];
-    followedUserPhotos = await getPhotos(user.userId, user.following)
+   const followedUserPhotos = await getPhotos(user.userId, user.following)
+      //re-arrange array by newest date created
     followedUserPhotos.sort((a, b) => b.dateCreated - a.dateCreated);
     setPhotos(followedUserPhotos);
   }
-  //re-arrange array by newest date created
+
 
 }
 getTimeLinePhotos();
